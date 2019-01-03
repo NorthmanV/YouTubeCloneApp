5fe4708cafac814d70e02412dc1a92c88cba659b
Ruslan Akberov <ruslanakberov@gmail.com>
2019-01-03 23:00:57 +0300

    Play-pause video implemented

--- YouTubeCloneApp/Views/VideoPlayerView.swift
+++ YouTubeCloneApp/Views/VideoPlayerView.swift
@@ -11,19 +11,77 @@ import AVFoundation
 
 class VideoPlayerView: UIView {
     
+    let controlsContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(white: 0, alpha: 1)
+        return view
+    }()
+    
+    let activityIndicatorView: UIActivityIndicatorView = {
+        let activity = UIActivityIndicatorView(style: .whiteLarge)
+        activity.startAnimating()
+        activity.translatesAutoresizingMaskIntoConstraints = false
+        return activity
+    }()
+    
+    let pausePlayButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "pause"), for: .normal)
+        button.tintColor = .white
+        button.addTarget(self, action: #selector(handlePause), for: .touchUpInside)
+        button.isHidden = true
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    var player: AVPlayer!
+    var isPlaying = false
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .black
         let url = URL(string: "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4")!
-        let player = AVPlayer(url: url)
+        player = AVPlayer(url: url)
         let playerLayer = AVPlayerLayer(player: player)
         layer.addSublayer(playerLayer)
         playerLayer.frame = frame
         player.play()
+        player.addObserver(self, forKeyPath: "currentItem.loadedTimeRanges", options: .new, context: nil)
+        
+        controlsContainerView.frame = frame
+        addSubview(controlsContainerView)
+        
+        controlsContainerView.addSubview(activityIndicatorView)
+        activityIndicatorView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        activityIndicatorView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        
+        controlsContainerView.addSubview(pausePlayButton)
+        pausePlayButton.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        pausePlayButton.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "currentItem.loadedTimeRanges" {
+            activityIndicatorView.stopAnimating()
+            controlsContainerView.backgroundColor = .clear
+            pausePlayButton.isHidden = false
+            isPlaying = true
+        }
+    }
+    
+    @objc func handlePause() {
+        if isPlaying {
+            player.pause()
+            pausePlayButton.setImage(UIImage(named: "play"), for: .normal)
+        } else {
+            player.play()
+            pausePlayButton.setImage(UIImage(named: "pause"), for: .normal)
+        }
+        isPlaying.toggle()
     }
     
 }
