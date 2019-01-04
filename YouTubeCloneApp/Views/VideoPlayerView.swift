1c65928ae20d075915d0ea7eb40b3db04dcb59d3
Ruslan Akberov <ruslanakberov@gmail.com>
2019-01-04 15:02:41 +0300

    Video rewinding implemented

--- YouTubeCloneApp/Views/VideoPlayerView.swift
+++ YouTubeCloneApp/Views/VideoPlayerView.swift
@@ -34,13 +34,34 @@ class VideoPlayerView: UIView {
         return button
     }()
     
+    let videoLengthLabel: UILabel = {
+        let label = UILabel()
+        label.text = "00:00"
+        label.textColor = .white
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.textAlignment = .right
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let videoSlider: UISlider = {
+        let slider = UISlider()
+        slider.minimumTrackTintColor = UIColor.rgb(red: 230, green: 32, blue: 31, alpha: 1)
+        slider.maximumTrackTintColor = .white
+        let image = UIImage(named: "circle")
+        slider.setThumbImage(image, for: .normal)
+        slider.addTarget(self, action: #selector(sliderValueChanged), for: .valueChanged)
+        slider.translatesAutoresizingMaskIntoConstraints = false
+        return slider
+    }()
+    
     var player: AVPlayer!
     var isPlaying = false
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .black
-        let url = URL(string: "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4")!
+        let url = URL(string: "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_5mb.mp4")!
         player = AVPlayer(url: url)
         let playerLayer = AVPlayerLayer(player: player)
         layer.addSublayer(playerLayer)
@@ -58,6 +79,15 @@ class VideoPlayerView: UIView {
         controlsContainerView.addSubview(pausePlayButton)
         pausePlayButton.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
         pausePlayButton.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        
+        controlsContainerView.addSubview(videoLengthLabel)
+        videoLengthLabel.rightAnchor.constraint(equalTo: rightAnchor, constant: -8).isActive = true
+        videoLengthLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -4).isActive = true
+        
+        controlsContainerView.addSubview(videoSlider)
+        videoSlider.rightAnchor.constraint(equalTo: videoLengthLabel.leftAnchor).isActive = true
+        videoSlider.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+        videoSlider.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -4).isActive = true
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -70,6 +100,12 @@ class VideoPlayerView: UIView {
             controlsContainerView.backgroundColor = .clear
             pausePlayButton.isHidden = false
             isPlaying = true
+            if let duration = player.currentItem?.duration {
+                let seconds = CMTimeGetSeconds(duration)
+                let secondsText =  String(format: "%02d", Int(seconds) % 60)
+                let minutesText = String(format: "%02d", Int(seconds) / 60)
+                videoLengthLabel.text = "\(minutesText):\(secondsText)"
+            }
         }
     }
     
@@ -82,6 +118,18 @@ class VideoPlayerView: UIView {
             pausePlayButton.setImage(UIImage(named: "pause"), for: .normal)
         }
         isPlaying.toggle()
+    }
+    
+    @objc func sliderValueChanged() {
+        if let duration = player.currentItem?.duration {
+            let totalSeconds = CMTimeGetSeconds(duration)
+            let value = Float64(videoSlider.value) * totalSeconds
+            let seekTime = CMTime(value: Int64(value), timescale: 1)
+            player.seek(to: seekTime) { (isCompleted) in
+                
+            }
+            
+        }
     }
     
 }
